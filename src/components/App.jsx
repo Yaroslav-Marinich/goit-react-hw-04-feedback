1846f4dd339e0ad765bffd3fcf1b7244da3619be
Yaroslav-Marinich <ysmarinich@gmail.com>
2023-05-15 10:50:49 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,57 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import { Container } from './Container/Container';
 import { Feedback } from './Feedback/Feedback';
 import { GlobalStyle } from './GlobalStyle';
 import { ErrorMsg } from './Error/Error';
 import { Statistics } from './Statistics/Statistics';
 
-export class App extends Component {
-  state = {
+export const App = () => {
+  const [state, setState] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  setStateUpdate = option => {
-    this.setState(prevState => ({
+  const setStateUpdate = option => {
+    setState(prevState => ({
+      ...prevState,
       [option]: prevState[option] + 1,
     }));
   };
 
-  countTotal = () => {
-    const values = Object.values(this.state);
+  const countTotal = () => {
+    const values = Object.values(state);
     return values.reduce((acc, value) => acc + value, 0);
   };
 
-  countPositivePercent = () => {
-    const { good } = this.state;
-    return Math.floor((good / this.countTotal()) * 100) || 0;
+  const countPositivePercent = () => {
+    const { good } = state;
+    return Math.floor((good / countTotal()) * 100) || 0;
   };
 
-  render() {
-    const keys = Object.keys(this.state);
-    const totalCount = this.countTotal();
+  const keys = Object.keys(state);
+  const totalCount = countTotal();
 
-    return (
-      <div>
-        <Container title="Please leave feedback">
-          <Feedback options={keys} onStateUpdate={this.setStateUpdate} />
-        </Container>
-        <Container title="Statistics">
-          {totalCount === 0 ? (
-            <ErrorMsg message="No feedback given" />
-          ) : (
-            <Statistics
-              options={keys}
-              statistic={this.state}
-              total={totalCount}
-              positiveFeedback={this.countPositivePercent}
-            />
-          )}
-        </Container>
-        <GlobalStyle />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Container title="Please leave feedback">
+        <Feedback options={keys} onStateUpdate={setStateUpdate} />
+      </Container>
+      <Container title="Statistics">
+        {totalCount === 0 ? (
+          <ErrorMsg message="No feedback given" />
+        ) : (
+          <Statistics
+            options={keys}
+            statistic={state}
+            total={totalCount}
+            positiveFeedback={countPositivePercent}
+          />
+        )}
+      </Container>
+      <GlobalStyle />
+    </div>
+  );
 }
+
+export default App;
